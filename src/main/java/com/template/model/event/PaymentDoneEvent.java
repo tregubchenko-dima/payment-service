2052c40a8cf6a1f5be494ca7c76ab254b64bb2c7
tregubchenko2@yandex.ru <tregubchenko>
2025-06-08 18:22:53 +0300

    init events

--- src/main/java/com/template/model/event/PaymentDoneEvent.java
+++ src/main/java/com/template/model/event/PaymentDoneEvent.java
@@ -3,10 +3,12 @@ package com.template.model.event;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.UUID;
 
 @Data
 public class PaymentDoneEvent {
 
+    private UUID orderId;
     private String orderName;
     private Integer price;
     private String status;
